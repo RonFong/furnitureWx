c359068dc32579229c6f976f37f4cc3a1ff90c87
mengshulin <229557322@qq.com>
2018-06-25 21:37:23 +0800

    'modify'

--- public/Eleditor/upload.php
+++ public/Eleditor/upload.php
@@ -2,7 +2,7 @@
 
 $result = json_encode([
     'status' => 1,
-    'url'    => '',
+    'url'    => 'http://img5.imgtn.bdimg.com/it/u=1126380040,3366465233&fm=27&gp=0.jpg',
     'msg'    => '成功',
     'data'   => $_FILES,
 ]);
