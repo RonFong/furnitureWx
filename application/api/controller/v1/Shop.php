2502a60a81b6caaa1b4eb6296cf3265e4c431676
lishhPhper <nietzsche_nc@163.com>
2018-07-02 10:00:52 +0800

    初始化Shop

--- application/api/controller/v1/Shop.php
+++ application/api/controller/v1/Shop.php
@@ -31,88 +31,11 @@ class Shop extends BaseController
 
         parent::__construct($request);
         $this->currentModel    = new shopModel();
+        $this->currentValidate = validate('shop');
     }
 
     public function register()
     {
-        // 请求参数
-        $request = $this->data;
-        // openid
-        if(empty($request['openid'])){
-            return $this->jsonReturn(0,'请先授权登录');
-        }
-        $openid = $request['openid'];
-        // 表单信息
-        // 门店名称
-        if(empty($request['shop_name']) || !is_string($request['shop_name'])){
-            return $this->jsonReturn(0,'请填门店名称');
-        }
-        $shop_name = trim($request['shop_name']);
-        // 法人姓名
-        if(empty($request['user_name']) || !is_string($request['user_name'])){
-            return $this->jsonReturn(0,'请填写法人姓名');
-        }
-        $user_name = trim($request['user_name']);
-        // 法人微信号
-        if(empty($request['wx_account']) || !is_string($request['wx_account'])){
-            return $this->jsonReturn(0,'请填写法人微信号');
-        }
-        $wx_account = trim($request['wx_account']);
-        // 门头照
-        if(empty($request['shop_img']) || !is_string($request['shop_img'])){
-            return $this->jsonReturn(0,'请上传门头照');
-        }
-        $shop_img = trim($request['shop_img']);
-        // 手机号
-        if(!isset($request['phone']) || !StringFilter::checkPhone($request['phone'])){
-            return $this->jsonReturn(0,'手机号码格式错误');
-        }
-        $phone = StringFilter::controlCharacter($request['phone']);
 
-        // 地址
-        if(empty($request['province']) || !is_numeric($request['province'])){
-            return $this->jsonReturn(0,'请选择省');
-        }
-        if(empty($request['city']) || !is_numeric($request['city'])){
-            return $this->jsonReturn(0,'请选择市');
-        }
-        if(empty($request['address']) || !is_string($request['address'])){
-            return $this->jsonReturn(0,'请填写详细地址');
-        }
-        $province = $request['province'];
-        $city = $request['city'];
-        $address = StringFilter::controlCharacter($request['address']);
-
-        // 根据openid获取用户id
-        $user = new User();
-        $user_record = $user->checkExistsUser($openid);
-        if(!$user_record){
-            // TODO 注册成为普通用户还是提示未注册
-        }
-
-        $admin_user = $user_record->id;
-        $now = time();
-        $record = [
-            'shop_name' => $shop_name,
-            'admin_user' => $admin_user,
-            'phone' => $phone,
-            'wx_account' => $wx_account,
-            'user_name' => $user_name,
-            'shop_img' => $shop_img,
-            'state' => 1,
-            'province' => $province,
-            'city' => $city,
-            'address' => $address,
-            'create_time' => $now,
-            'create_by' => $now,
-            'update_time' => $now,
-            'update_by' => $now,
-        ];
-
-        $shopInfo = $this->currentModel->saveData($record);
-        if (!$shopInfo) {
-            return $this->jsonReturn(0, '提交失败，请重试');
-        }
-        return $this->jsonReturn(1, '提交成功');
     }
 }
---
+++ application/api/validate/Shop.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\api\validate;
+
+use app\common\validate\Shop as CoreShop;
+
+class Shop extends CoreShop
+{
+
+}
