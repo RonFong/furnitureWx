d3ab203ccb981ad6097a0b7b88523cd9e386e855
mengshulin <229557322@qq.com>
2018-07-04 16:22:12 +0800

    modify

--- public/Eleditor/demo/index.php
+++ public/Eleditor/demo/index.php
@@ -1,3 +1,8 @@
+<?php
+require_once "jssdk.php";
+$jssdk       = new JSSDK("wx195a5e8ed1a55ead", "7a91a512262ec4a9dea5a507612e48a0");
+$signPackage = $jssdk->GetSignPackage();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +13,7 @@
     <!-- 引入jQuery -->
     <script src="../jquery.min.js"></script>
 
-    <script src="../webuploader.min.js"></script>
+<!--    <script src="../webuploader.min.js"></script>-->
     <!-- 插件核心 -->
     <script src="../Eleditor.min.js"></script>
 
@@ -104,20 +109,81 @@
 
 <script>
     wx.config({
+        debug    : true,
+        appId    : '<?php echo $signPackage["appId"];?>',
+        timestamp: <?php echo $signPackage["timestamp"];?>,
+        nonceStr : '<?php echo $signPackage["nonceStr"];?>',
+        signature: '<?php echo $signPackage["signature"];?>',
+        jsApiList: [
+            // 所有要调用的 API 都要加到这个列表中
+            'chooseImage',
+            'previewImage',
+            'uploadImage',
+            'downloadImage'
+        ]
+    });
+    wx.ready(function () {
+        // 在这里调用 API
+        wx.checkJsApi({
+            jsApiList: [
+                'checkJsApi',
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage',
+                'onMenuShareQQ',
+                'onMenuShareWeibo',
+                'onMenuShareQZone',
+                'hideMenuItems',
+                'showMenuItems',
+                'hideAllNonBaseMenuItem',
+                'showAllNonBaseMenuItem',
+                'translateVoice',
+                'startRecord',
+                'stopRecord',
+                'onVoiceRecordEnd',
+                'playVoice',
+                'onVoicePlayEnd',
+                'pauseVoice',
+                'stopVoice',
+                'uploadVoice',
+                'downloadVoice',
+                'chooseImage',
+                'previewImage',
+                'uploadImage',
+                'downloadImage',
+                'getNetworkType',
+                'openLocation',
+                'getLocation',
+                'hideOptionMenu',
+                'showOptionMenu',
+                'closeWindow',
+                'scanQRCode',
+                'chooseWXPay',
+                'openProductSpecificView',
+                'addCard',
+                'chooseCard',
+                'openCard'
+            ],
+            success: function (res) {
+                if (res.checkResult.getLocation == false) {
+                    alert('你的微信版本太低，不支持微信JS接口，请升级到最新的微信版本！');
+                    return;
+                }else{
+                    wx.chooseImage({
+                        count: 1,
+                        sizeType: ['compressed'],
+                        success: function (_selected) {
 
-        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-
-        appId: 'wxa6d691299093f1a4', // 必填，公众号的唯一标识
-
-        timestamp: Date.parse(new Date()), // 必填，生成签名的时间戳
-
-        nonceStr: '', // 必填，生成签名的随机串
-
-        signature: '',// 必填，签名，见附录1
-
-        jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-
-    })
+                        }
+                    });
+                }
+            }
+        });
+    });
+    wx.error(function(res){
+        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+        alert("验证失败，请重试！");
+        //wx.closeWindow();
+    });
     var contentEditor = new Eleditor({
         el      : '#contentEditor',
         // upload  : {
@@ -138,7 +204,6 @@
                     count: 1,
                     sizeType: ['compressed'],
                     success: function (_selected) {
-                        console.log(_selected);
                         if( _selected.localIds.length == 0 ){
                             return;
                         }
@@ -158,7 +223,6 @@
                                     },
                                     cache: false,
                                     success: function(_resu){
-
                                         if( _resu.status == 0 ){
                                             return _reject(_resu.msg);
                                         }
@@ -178,39 +242,39 @@
             });
         },
         /*初始化完成钩子*/
-        mounted : function () {
-
-            /*以下是扩展插入视频的演示*/
-            var _videoUploader = WebUploader.create({
-                auto     : true,
-                server   : '服务器地址',
-                /*按钮类就是[Eleditor-你的自定义按钮id]*/
-                pick     : $('.Eleditor-insertVideo'),
-                duplicate: true,
-                resize   : false,
-                accept   : {
-                    title     : 'Images',
-                    extensions: 'mp4',
-                    mimeTypes : 'video/mp4'
-                },
-                fileVal  : 'video',
-            });
-            _videoUploader.on('uploadSuccess', function (_file, _call) {
-
-                if (_call.status == 0) {
-                    return window.alert(_call.msg);
-                }
-
-                /*保存状态，以便撤销*/
-                contentEditor.saveState();
-                contentEditor.getEditNode().after(`
-									<div class='Eleditor-video-area'>
-										<video src="${_call.url}" controls="controls"></video>
-									</div>
-								`);
-                contentEditor.hideEditorControllerLayer();
-            });
-        },
+        // mounted : function () {
+        //
+        //     /*以下是扩展插入视频的演示*/
+        //     var _videoUploader = WebUploader.create({
+        //         auto     : true,
+        //         server   : '服务器地址',
+        //         /*按钮类就是[Eleditor-你的自定义按钮id]*/
+        //         pick     : $('.Eleditor-insertVideo'),
+        //         duplicate: true,
+        //         resize   : false,
+        //         accept   : {
+        //             title     : 'Images',
+        //             extensions: 'mp4',
+        //             mimeTypes : 'video/mp4'
+        //         },
+        //         fileVal  : 'video',
+        //     });
+        //     _videoUploader.on('uploadSuccess', function (_file, _call) {
+        //
+        //         if (_call.status == 0) {
+        //             return window.alert(_call.msg);
+        //         }
+        //
+        //         /*保存状态，以便撤销*/
+        //         contentEditor.saveState();
+        //         contentEditor.getEditNode().after(`
+			// 						<div class='Eleditor-video-area'>
+			// 							<video src="${_call.url}" controls="controls"></video>
+			// 						</div>
+			// 					`);
+        //         contentEditor.hideEditorControllerLayer();
+        //     });
+        // },
         changer : function () {
             console.log('文档修改');
         },
@@ -218,20 +282,7 @@
         toolbars: [
             'insertText',
             'editText',
-            // 'insertImage',
-            {
-                id    : 'insertImage',
-                // tag: 'p,img', //指定P标签操作，可不填
-                name  : '插入图片',
-                handle: function (select, controll) {//回调返回选择的dom对象和控制按钮对象
-                    console.log(22222222222222);
-                    contentEditor.uploader()
-                    /*因为上传要提前绑定按钮到webuploader，所以这里不做上传逻辑，写在mounted*/
-
-                    /*!!!!!!返回false编辑面板不会关掉*/
-                    return false;
-                }
-            },
+            'insertImage',
             'insertLink',
             'insertHr',
             'delete',
@@ -241,8 +292,7 @@
                 // tag: 'p,img', //指定P标签操作，可不填
                 name  : '插入视频',
                 handle: function (select, controll) {//回调返回选择的dom对象和控制按钮对象
-                    console.log(11111111111111);
-                    this.uploader()
+
                     /*因为上传要提前绑定按钮到webuploader，所以这里不做上传逻辑，写在mounted*/
 
                     /*!!!!!!返回false编辑面板不会关掉*/
