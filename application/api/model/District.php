d280d60bf61d02f74deb53863bde125efadb6e68
lishhPhper <nietzsche_nc@163.com>
2018-07-09 16:07:10 +0800

    modify

--- application/api/model/District.php
+++ application/api/model/District.php
@@ -10,11 +10,11 @@ class District extends CoreDistrict
     public function getRegionData($parent_id = '',$level = '')
     {
         $data = $this->field(['id','name'])->where('parent_id',$parent_id)->where('level',$level)->select();
-        $data = collection($data)->toArray();
-        $data   = array_column($data,'name','id');  //键值
-        $result['ids'] = array_keys($data);
-        $result['region'] = $data;
-        return $result;
+//        $data = collection($data)->toArray();
+//        $data   = array_column($data,'name','id');  //键值
+//        $result['ids'] = array_keys($data);
+//        $result['region'] = $data;
+        return $data;
 //        var_dump($result);die;
     }
 }
