104444950e84f18bfaf9ef78e8132d759d3bac57
lixiaolong <lishalom@sina.com>
2018-07-15 18:01:12 +0800

    图片统一上传接口

--- application/api/controller/v1/Image.php
+++ application/api/controller/v1/Image.php
@@ -13,6 +13,7 @@ namespace app\api\controller\v1;
 
 use app\api\controller\BaseController;
 use think\Request;
+use think\Image as ThinkImage;
 
 /**
  * 保存图片资源
@@ -21,20 +22,89 @@ use think\Request;
  */
 class Image extends BaseController
 {
+    /**
+     * 临时图片存储地址
+     * @var string
+     */
+    protected $imgPath;
+
+    /**
+     * 前端图片调用地址
+     * @var string
+     */
+    protected $viewImgPath;
+
+    /**
+     * 缩略图后缀
+     * @var string
+     */
+    protected $thumbImgSuffix = '_thumb';
+
+    /**
+     * 缩略图尺寸
+     * @var string
+     */
+    protected $thumbImgSize = [
+        'width'     => 150,
+        'height'    => 150
+    ];
+
     public function __construct(Request $request = null)
     {
         parent::__construct($request);
+        $this->imgPath = IMAGE_PATH . "/tmp/";
+        $this->viewImgPath = VIEW_IMAGE_PATH . "/tmp/";
     }
 
 
+    /**
+     * @api {post} /v1/image/temporary  临时存储图片 (base64格式)
+     * @apiGroup image
+     *
+     * @apiParamExample  {string} 请求参数格式：
+     * {
+     *      "img":"data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAMCAgMCAgMDAwMEAwMEBQgFBQQEBQoHBwYIDAoMDA********"
+     * }
+     *
+     * @apiSuccessExample {json} 成功时的返回：
+     * {
+     *      "data": "/static/img/tmp/f70cc04e10d49e0dfc8ea49029d38593.jpeg"
+     * }
+     */
     public function saveTmpImg()
     {
-        if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $this->data['img'], $result)){
-            $imgFile = IMAGE_PATH . "/tmp/" . md5(uniqid(microtime(true))) . "." . $result[2];
-            if (file_put_contents($imgFile, base64_decode(str_replace($result[1], '', $this->data['img'])))){
-                echo '新文件保存成功：', $imgFile;
-            }
 
+        try {
+            if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $this->data['img'], $result)){
+                //生成图片名
+                $imgName = md5(uniqid(microtime(true)));
+                //写入到目标文件
+                $imgFile = $this->imgPath . $imgName . '.' .$result[2];
+                if (file_put_contents($imgFile, base64_decode(str_replace($result[1], '', $this->data['img'])))){
+                    //生成后缀为 $this->thumbImgSuffix 的缩略图
+                    $image = ThinkImage::open($imgFile);
+                    if ($image->width() < $this->thumbImgSize['width'] || $image->height() < $this->thumbImgSize['height']) {
+                        //原图尺寸小于预定缩略图，则固定尺寸缩放裁剪
+                        $thumbType = ThinkImage::THUMB_FIXED;
+                    } else {
+                        //居中缩放裁剪
+                        $thumbType = ThinkImage::THUMB_CENTER;
+                    }
+                    //保存缩略图
+                    $image->thumb($this->thumbImgSize['width'], $this->thumbImgSize['height'], $thumbType)
+                        ->save($this->imgPath . $imgName . $this->thumbImgSuffix . '.' . $result[2]);
+
+                    $this->result['data'] = [
+                        'img'       => $this->viewImgPath . $imgName . '.' .$result[2],
+                        'img_thumb' => $this->viewImgPath . $imgName . $this->thumbImgSuffix . '.' .$result[2]
+                    ];
+                }
+            }
+        } catch (\Exception $e) {
+            $code = 400;
+            $this->result['state'] = 0;
+            $this->result['msg'] = $e->getMessage();
         }
+        return json($this->result, $code ?? 200);
     }
 }
