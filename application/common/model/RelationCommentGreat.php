d9d499705620e635615b385406fd33c864a37a99
lixiaolong <lishalom@sina.com>
2018-07-17 00:57:55 +0800

    add

---
+++ application/common/model/RelationCommentGreat.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 黎小龙 <shalinglom@gmail.com>
+// +----------------------------------------------------------------------
+// | CreateTime: 2018/7/16 
+// +----------------------------------------------------------------------
+
+
+namespace app\common\model;
+use traits\model\SoftDelete;
+
+/**
+ * 用户点赞评论
+ * Class RelationCommentGreat
+ * @package app\common\model
+ */
+class RelationCommentGreat extends Model
+{
+    use SoftDelete;
+}
---
+++ application/common/model/RelationGoodsCollect.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 黎小龙 <shalinglom@gmail.com>
+// +----------------------------------------------------------------------
+// | CreateTime: 2018/7/16 
+// +----------------------------------------------------------------------
+
+
+namespace app\common\model;
+use traits\model\SoftDelete;
+
+/**
+ * 用户收藏商城商品
+ * Class RelationGoodsLike
+ * @package app\common\model
+ */
+class RelationGoodsCollect extends Model
+{
+    use SoftDelete;
+}
