8ff857f240ccf21055e5b3d16d6e6f01bde5b859
lixiaolong <lishalom@sina.com>
2018-07-17 00:57:37 +0800

    添加 收藏、点赞、关注 统一处理类

---
+++ application/api/service/Relate.php
@@ -0,0 +1,67 @@
+<?php
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 黎小龙 <shalinglom@gmail.com>
+// +----------------------------------------------------------------------
+// | CreateTime: 2018/7/16 
+// +----------------------------------------------------------------------
+
+
+namespace app\api\service;
+
+/**
+ * 产生关联
+ * Class Relate
+ * @package app\api\service
+ */
+class Relate
+{
+    /**
+     * 当前操作的模型
+     * @var object
+     */
+    protected $behaviorModel;
+
+    public function __construct($behaviorModel)
+    {
+        try {
+            $class =  '\\app\\common\\model\\' . $behaviorModel;
+            $class = new \ReflectionClass($class);
+            $this->behaviorModel = $class->newInstanceArgs();
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function save($data)
+    {
+        $method = $data['type'];
+        return $this->$method($data);
+    }
+
+    /**
+     * 增长
+     * @param $data
+     * @return string
+     */
+    protected function inc($data)
+    {
+        $data['create_date'] = date('Ymd', time());
+        $data['create_time'] = time();
+        $result = $this->behaviorModel->save($data);
+        return $result ?? false;
+    }
+
+    /**
+     * 减少
+     * @param $data
+     * @return bool
+     */
+    protected function dec($data)
+    {
+        unset($data['type']);
+        $result = $this->behaviorModel->where($data)->update(['delete_time' => time()]);
+        return $result ?? false;
+    }
+}
