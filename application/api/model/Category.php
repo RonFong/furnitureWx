1c443098f1207ff85e746ead89f82ec451b7b411
lishhPhper <nietzsche_nc@163.com>
2018-07-19 18:43:17 +0800

    经营分类初步

--- application/api/model/Category.php
+++ application/api/model/Category.php
@@ -15,14 +15,28 @@ class Category extends CoreCategory
         $all = collection($all)->toArray();
         //$items[$item['pid']]['son'][$item['id']] = &$items[$item['id']];
         //    return isset($items[0]['son']) ? $items[0]['son'] : array();
-        $tree = array();
-        foreach ($all as $key => $item){
-            if (isset($all[$item['parent_id']]))
-                $all[$item['parent_id']]['son'][] = &$all[$item['id']];
-            else
-                $tree[] = &$all[$item['id']];
-            return $tree;
-        }
+//        $tree = array();
+//        foreach ($all as $key => $item){
+//            if (isset($all[$item['parent_id']]))
+//                $all[$item['parent_id']]['son'][] = &$all[$item['id']];
+//            else
+//                $tree[] = &$all[$item['id']];
+//        }
+        $tree = $this->getTree($all,0);
         dump($tree);die;
+    }
+
+    public function getTree($data, $pId)
+    {
+        $tree = '';
+        foreach($data as $k => $v)
+        {
+            if($v['parent_id'] == $pId)
+            {        //父亲找到儿子
+                $v['parent_id'] = $this->getTree($data, $v['parent_id']);
+                $tree[] = $v;
+            }
+        }
+        return $tree;
     }
 }
