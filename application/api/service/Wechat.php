bdb7ed8fc7773259bb24f4786cf3a2efa91e5cc1
lom <lishalom@sina.com>
2018-07-30 16:43:26 +0800

    update

--- application/api/service/Wechat.php
+++ application/api/service/Wechat.php
@@ -35,7 +35,9 @@ class Wechat {
         $jsCode = $data['code'];
         $url         = $url = "https://api.weixin.qq.com/sns/jscode2session?appid=$this->appid&secret=$this->appSecret&js_code=$jsCode&grant_type=authorization_code";
         $info = json_decode(curl_get($url));
-
+        if (!isset($info->openid)) {
+            exception('openid获取失败:' . $info->errmsg);
+        }
         //Cache::set('session_key:'.$info->openid,$info->session_key);
         return $info->openid;
     }
