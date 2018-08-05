2768640f592f86a6502ad5832a1c050045069872
zhang <wei_06@126.com>
2018-08-05 17:58:40 +0800

    后台

--- application/admin/controller/GroupClassify.php
+++ application/admin/controller/GroupClassify.php
@@ -20,6 +20,99 @@ class GroupClassify extends Base
         $this->currentModel = new CoreGroupClassify();//实例化当前模型
     }
 
+    /**
+     * 列表页
+     * @return mixed
+     */
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * 列表页，获取数据
+     * @return mixed
+     */
+    public function getDataList()
+    {
+        $map = $this->getDataListMap();
+        return $this->currentModel->where($map)->order('id desc')->layTable();
+    }
+
+    private function getDataListMap()
+    {
+        $param = $this->request->param();
+        if (!empty($param['factory_name'])) {
+            $map['factory_name'] = ['like', '%' . $param['factory_name'] . '%'];//厂家名
+        }
+        if (!empty($param['factory_phone'])) {
+            $map['factory_phone'] = ['like', '%' . $param['factory_phone'] . '%'];//手机号
+        }
+        if (!empty($param['factory_contact'])) {
+            $map['factory_contact'] = ['like', '%' . $param['factory_contact'] . '%'];//联系人
+        }
+        if (empty($map)) {
+            $map[] = ['exp', '1=1'];
+        }
+        return $map;
+    }
+
+    /**
+     * 编辑
+     * @return mixed
+     * @throws \think\exception\DbException
+     */
+    public function edit()
+    {
+        $param = $this->request->param();
+
+        if (!empty($param['id'])) {
+            $data = $this->currentModel->where('id', $param['id'])->find();
+            if (empty($data)) {
+                $this->error('信息不存在');
+            }
+            $data = $data->toArray();
+            $this->assign('data', $data);
+
+            /*获取下拉列表：市*/
+            $city = $this->getRegion($data['province']);
+            $this->assign('cityList', $city);
+
+            /*获取下拉列表：区/镇*/
+            $district = $this->getRegion($data['city']);
+            $this->assign('districtList', $district);
+        }
+
+        /*获取下拉列表：省份*/
+        $provinceList = $this->getRegion(0);
+        $this->assign('provinceList', $provinceList);
+
+        return $this->fetch();
+    }
+
+    public function save()
+    {
+        $param = $this->request->param();//获取请求数据
+
+        if (empty($param)) {
+            $this->error('没有需要保存的数据！');
+        }
+
+        //验证数据
+        $result = $this->validate($param, 'User');
+        if ($result !== true) {
+            $this->error($result);
+        }
+
+        try {
+            //保存数据
+            $this->currentModel->save($param);
+        } catch (\Exception $e) {
+            $msg = !empty($this->currentModel->getError()) ? $this->currentModel->getError() : $e->getMessage();
+            $this->error($msg);
+        }
+        $this->success('保存成功！', 'edit?id='.$this->currentModel->id);
+    }
 
 
 }
