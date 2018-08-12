179f005f9aaaa3984e0aaa870813da52a8926739
lixiaolong <lishalom@sina.com>
2018-08-12 12:43:53 +0800

    删除数据库链接信息

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '47.98.223.33',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'furniture',
     // 用户名
-    'username'        => 'user',
+    'username'        => 'root',
     // 密码
-    'password'        => 'lucky2018',
+    'password'        => 'root',
     // 端口
     'hostport'        => '',
     // 连接dsn
