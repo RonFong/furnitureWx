d9d1b949e34ffa21aa59a475ba1944a08c5739a8
lixiaolong <lishalom@sina.com>
2018-08-12 19:00:44 +0800

    update

--- application/common/model/RelationArticleGreat.php
+++ application/common/model/RelationArticleGreat.php
@@ -9,7 +9,6 @@
 
 
 namespace app\common\model;
-use traits\model\SoftDelete;
 
 /**
  * 用户点赞文章
@@ -18,5 +17,5 @@ use traits\model\SoftDelete;
  */
 class RelationArticleGreat extends Model
 {
-    use SoftDelete;
+    
 }
