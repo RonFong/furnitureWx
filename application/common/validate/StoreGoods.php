565f7e1dc72a9b9938cf54c54a5c83fb80caf8c4
lixiaolong <lishalom@sina.com>
2018-08-12 19:01:54 +0800

    add

---
+++ application/common/validate/StoreGoods.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 黎小龙 <shalinglom@gmail.com>
+// +----------------------------------------------------------------------
+// | CreateTime: 2018/8/12 
+// +----------------------------------------------------------------------
+
+
+namespace app\common\validate;
+
+/**
+ * 商城商品
+ * Class StoreGoods
+ * @package app\common\validate
+ */
+class StoreGoods extends BaseValidate
+{
+
+}
