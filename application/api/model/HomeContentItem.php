4c24b1c49fd266aa44e95ccba33936645481a80b
mengshulin <229557322@qq.com>
2018-08-22 10:35:28 +0800

    缓存图文

--- application/api/model/HomeContentItem.php
+++ application/api/model/HomeContentItem.php
@@ -26,7 +26,7 @@ class HomeContentItem extends CoreHomeContentItem
         if (!empty($contentData)) {
             $contentId       = $contentData[0]['id'];
             $contentItemData = Db::query("SELECT id,text,img FROM `home_content_item` WHERE content_id = {$contentId} ORDER BY sort DESC,id ASC");
-            $result = $contentData[0];
+            $result          = $contentData[0];
             if (!empty($contentItemData)) {
                 $result['items'] = $contentItemData;
             }
@@ -37,12 +37,14 @@ class HomeContentItem extends CoreHomeContentItem
 
     public static function setCache($data)
     {
-        $groupId     = $data['groupId'];
-        $groupType   = $data['groupType'];
-        $cacheData = Cache::get('home_content_cache_'.$groupId.'_'.$groupType);
-        $cacheData = json_decode($cacheData,true);
-        var_dump($cacheData);die;
-        Cache::set('home_content_cache_'.$data['itemId'], $data['text']);
+
+        $groupId   = $data['groupId'];
+        $groupType = $data['groupType'];
+        $cacheData = Cache::get('home_content_cache_' . $groupId . '_' . $groupType);
+        $cacheData = json_decode($cacheData, true);
+        var_dump($cacheData);
+        die;
+        Cache::set('home_content_cache_' . $data['itemId'], $data['text']);
 
         return true;
     }
@@ -50,23 +52,23 @@ class HomeContentItem extends CoreHomeContentItem
     public static function getCache($data)
     {
 
-        $result = Cache::get('home_content_cache_'.$data['itemId']);
+        $result = Cache::get('home_content_cache_' . $data['itemId']);
 
         return $result;
     }
 
     public static function saveContent($data)
     {
-        $groupId     = $data['groupId'];
-        $groupType   = $data['groupType'];
-        $music       = $data['music'];
-        $record      = $data['record'];
-        $musicName   = $data['musicName'];
-        $items       = json_decode($data['items'],true);
-        Db::query("UPDATE `home_content` SET music='{$music}',record='{$record}',music_name='{$musicName}' WHERE group_id = {$groupId} AND group_type = {$groupType}");
 
-        if(!empty($items)){
-            foreach ($items AS $key=>$value){
+        $groupId   = $data['groupId'];
+        $groupType = $data['groupType'];
+        $music     = $data['music'];
+        $record    = $data['record'];
+        $musicName = $data['musicName'];
+        $items     = json_decode($data['items'], true);
+        Db::query("UPDATE `home_content` SET music='{$music}',record='{$record}',music_name='{$musicName}' WHERE group_id = {$groupId} AND group_type = {$groupType}");
+        if (!empty($items)) {
+            foreach ($items AS $key => $value) {
                 $itemId = $value['id'];
                 $text   = $value['text'];
                 $img    = $value['img'];
@@ -74,8 +76,13 @@ class HomeContentItem extends CoreHomeContentItem
                 Db::query("UPDATE `home_content_item` SET text='{$text}',img='{$img}' WHERE id = {$itemId}");
             }
         }
-
-        Cache::set('home_content_cache_'.$groupId.'_'.$groupType, json_encode($data));
+        $cacheData = [
+            'music'     => $music,
+            'record'    => $record,
+            'musicName' => $musicName,
+            'items'     => $items,
+        ];
+        Cache::set('home_content_cache_' . $groupId . '_' . $groupType, json_encode($cacheData));
 
         return true;
     }
