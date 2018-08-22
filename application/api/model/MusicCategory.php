d2974f4d09c46197be176a442b7306c9285f3e98
lixiaolong <lishalom@sina.com>
2018-08-23 00:01:09 +0800

    添加音乐库接口

---
+++ application/api/model/MusicCategory.php
@@ -0,0 +1,19 @@
+<?php
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 黎小龙 <shalinglom@gmail.com>
+// +----------------------------------------------------------------------
+// | CreateTime: 2018/8/22 
+// +----------------------------------------------------------------------
+
+
+namespace app\api\model;
+
+
+use app\common\model\MusicCategory as CoreMusicCategory;
+
+class MusicCategory extends CoreMusicCategory
+{
+
+}
