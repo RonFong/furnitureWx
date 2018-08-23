9fc759f05014a32e21bdee921018a3234a537006
“g229557322@qq.com <229557322@qq.com>
2018-08-23 18:20:09 +0800

    modify

--- public/Eleditor/Eleditor.js
+++ public/Eleditor/Eleditor.js
@@ -129,9 +129,9 @@
 								<div class="Eleditor-textStyle-align Eleditor-textStyle-alignRight" align="right">\
 								</div>\
 							</div>\
-							<div class="Eleditor-textStyle-item Eleditor-textStyle-item-upImg">\
+							<!--<div class="Eleditor-textStyle-item Eleditor-textStyle-item-upImg">\
 								<div class="Eleditor-textStyle-upImg"></div>\
-							</div>\
+							</div>\-->\
 						</div>\
 						<div class="Eleditor-textEditor-colors">\
 							<div class="Eleditor-textEditor-modulePane"><span></span></div>\
@@ -498,8 +498,6 @@
 			var _uploaderHanlder = function(){
 
 				var _hasStyleBarBtn = $(this).hasClass('Eleditor-textStyle-item-upImg');
-				// 额外添加
-                $(this).addClass('Eleditor-textStyle-item-upImg-disabled');
 				
 				try{
 					_args.uploader.call(this).then(function(_url){
