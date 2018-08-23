10851ecb56b0c411f3f633817bc433c04ff2546c
lishhPhper <nietzsche_nc@163.com>
2018-08-23 15:33:56 +0800

    产品分类

--- application/common/model/GroupClassify.php
+++ application/common/model/GroupClassify.php
@@ -7,34 +7,44 @@ class GroupClassify extends Model
 {
     public function getClassifyList($group_id,$group_type)
     {
-//        $group_id = 2;
-//        $group_type = 1;
+        $group_id = 2;
+        $group_type = 1;
         $result = $this
             ->field(['id','parent_id','classify_name'])
             ->where('group_id',$group_id)
             ->where('group_type',$group_type)
             ->order(['sort' => 'desc'])
             ->select();
-        $three = array_values($this->formatTree($result,0));
-        $classify_data = [
-            '0' => [
-                'id' => 0,
-                'parent_id' => 0,
-                'classify' => '无',
-                'son' => []
-            ]
-        ];
-        $data = array_merge($classify_data,$three);
+//        dump(collection($result)->toArray());die;
+//        $data = \Tree::getTree($result,$name='classify_name',$id='id',$pid='parent_id');
+//        dump($data);die;
+        $tree =$this->formatTree($result,0);
+        $data = $this->_picker_tree_data($tree);
         return $data;
     }
 
     public function formatTree($arr,$pid=0){
+        $data = [];
         foreach($arr as $k => $v){
             if($v['parent_id']==$pid){
                 $data[$v['id']]=$v;
-                $data[$v['id']]['son']= array_values($this->formatTree($arr,$v['id']));
+                $data[$v['id']]['son']= $this->formatTree($arr,$v['id']);
             }
         }
         return isset($data)?$data:array();
+    }
+
+    public function _picker_tree_data($tree)
+    {
+        $result = ['0' => '无'];
+        foreach ($tree as $id => $item){
+            $result[$id] = $item['classify_name'];
+            if(!empty($item['son'])){
+                foreach ($item['son'] as $son_id => $value){
+                    $result[$son_id] = '|--'.$value['classify_name'];
+                }
+            }
+        }
+        return $result;
     }
 }
