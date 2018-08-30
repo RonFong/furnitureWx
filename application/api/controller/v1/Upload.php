1f071dd2ae1b5b30ae865d288801f1b8c3773910
“g229557322@qq.com <229557322@qq.com>
2018-08-30 10:00:06 +0800

    upload

---
+++ application/api/controller/v1/Upload.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/29
+ * Time: 18:24
+ */
+namespace app\api\controller\v1;
+
+use app\api\controller\BaseController;
+
+class Upload extends BaseController
+{
+    // 录音上传
+    public function recordUpload(){
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('image');
+
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        if($file){
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+            if($info){
+                // 成功上传后 获取上传信息
+                // 输出 jpg
+                echo $info->getExtension();
+                // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+                echo $info->getSaveName();
+                // 输出 42a79759f284b767dfcb2a0197904287.jpg
+                echo $info->getFilename();
+            }else{
+                // 上传失败获取错误信息
+                echo $file->getError();
+            }
+        }
+    }
+}
