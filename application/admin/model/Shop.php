6d1b5b9e24dbff3dc7a2894ea1e9a1ed2a34bb64
lom <shalinglom@sina.com>
2018-10-06 13:47:40 +0800

    添加列表页显示字段

--- application/admin/model/Shop.php
+++ application/admin/model/Shop.php
@@ -13,7 +13,20 @@ use app\common\model\Shop as CoreShop;
 
 class Shop extends CoreShop
 {
+    public function getAdminUserNameAttr($value, $data)
+    {
+        $user = User::get($value);
+        return $user->user_name;
+    }
 
+    public function getStateDesAttr($value)
+    {
+        return $value ? '启用' : '禁用';
+    }
 
-
+    public function getAuditStateDesAttr($value)
+    {
+        $array = ['未审核', '通过', '不通过'];
+        return $array[$value];
+    }
 }
