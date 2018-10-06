6b1523d3569c52ba755eafd4cbdfc99fa83b38af
lom <shalinglom@sina.com>
2018-10-06 23:28:02 +0800

    显示创建人信息

--- application/admin/model/Factory.php
+++ application/admin/model/Factory.php
@@ -10,10 +10,18 @@
 namespace app\admin\model;
 
 use app\common\model\Factory as CoreFactory;
+use think\Db;
 
 class Factory extends CoreFactory
 {
+    public function getAdminUserNameAttr($value, $data)
+    {
+        return Db::table('user')->where('id', $data['admin_user'])->value('user_name');
+    }
 
-
-
+    public function getAuditStateTextAttr($value, $data)
+    {
+        $array = ['未审核', '通过', '未通过'];
+        return $array[$data['audit_state']];
+    }
 }
