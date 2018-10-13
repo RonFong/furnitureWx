df138f64d69e095289fe5f596c4ccc0d53e1241b
lom <lishalom@sina.com>
2018-10-13 12:08:10 +0800

    保存微信用户的昵称和头像信息

--- application/api/service/Token.php
+++ application/api/service/Token.php
@@ -31,33 +31,43 @@ class Token
     /**
      * 获取 token 和用户信息
      * @param $openid
-     * @return Token|null
+     * @param $wxUserInfo
+     * @return Token|array|null
      */
-    public static function getToken($openid)
+    public static function getToken($openid, $wxUserInfo)
     {
         if (!$openid) {
             exception('缺少参数：openid');
         }
         self::$openid = $openid;
-        $userInfo = self::getUserInfo();
+        $userInfo = self::getUserInfo($wxUserInfo);
         $userInfo['token'] = self::createToken($userInfo['id']);
         return $userInfo;
     }
 
     /**
      * 此微信用户未注册，则注册，返回用户信息
+     * @param $wxUserInfo array 用户的微信信息
      * @return array|null|static
      */
-    private static function getUserInfo()
+    private static function getUserInfo($wxUserInfo)
     {
         $userInfo = User::get(['wx_openid' => self::$openid]);
+
         if (!$userInfo) {
+
+            $userName = $wxUserInfo['nickName'] ?? 'wx_' . substr(str_shuffle(self::$openid), 0, 6);
+            $city = $wxUserInfo['city'] ? Db::table('district')->where('pinyin', strtolower($wxUserInfo['city']))->value('name') : '';
+            $province = $wxUserInfo['province'] ? Db::table('district')->where('pinyin', strtolower($wxUserInfo['province']))->value('name') : '';
+
             $saveData = [
-                'wx_openid' => self::$openid,
-                'avatar'    => config('api.default_avatar'),
-                'user_name' => 'wx_' . substr(str_shuffle(self::$openid), 0, 6),
+                'wx_openid'     => self::$openid,
+                'avatar'        => $wxUserInfo['avatarUrl'] ?? config('api.default_avatar'),
+                'user_name'     => $userName,
                 'group_id'      => 0,
-                'gender'        => 0,
+                'gender'        => $wxUserInfo['gender'] ?? 0,
+                'city'          => $city,
+                'province'      => $province,
                 'phone'         => '',
                 'wx_account'    => '',
                 'type'          => 3,
