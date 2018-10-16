756d974ff321850794272eeb26227f04f588ab06
lom <lishalom@sina.com>
2018-10-16 17:09:57 +0800

    default

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '47.98.223.33',
+    'hostname'        => 'localhost',
     // 数据库名
     'database'        => 'furniture',
     // 用户名
