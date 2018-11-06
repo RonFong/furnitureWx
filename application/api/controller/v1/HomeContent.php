48eb3f3adcc65c84648aee1f25eb4573501dc6fc
lixiaolong <lishalom@sina.com>
2018-11-07 01:21:07 +0800

    首页图文

--- application/api/controller/v1/HomeContent.php
+++ application/api/controller/v1/HomeContent.php
@@ -3,129 +3,66 @@ namespace app\api\controller\v1;
 
 use app\api\controller\BaseController;
 use app\api\model\HomeContent as homeContentModel;
-use app\api\model\HomeContentItem;
+use app\common\validate\HomeContent as homeContentValidate;
 use app\lib\enum\Response;
-use think\Cache;
 use think\Request;
 
 class HomeContent extends BaseController
 {
 
-    /**
-     * 参数校验统一入口方法
-     * HomeContent constructor.
-     * @param Request|null $request
-     */
     public function __construct(Request $request = null)
     {
 
         parent::__construct($request);
         $this->currentModel    = new homeContentModel();
-        $this->currentValidate = validate('homeContent');
+        $this->currentValidate = new homeContentValidate();
     }
 
-    public function addHomeContent()
+    /**
+     * 创建图文
+     * @throws \app\lib\exception\BaseException
+     */
+    public function create()
     {
-
-        $homeContentModel             = new homeContentModel();
-        $homeContentModel->group_id   = 2;
-        $homeContentModel->group_type = 1;
-        $homeContentModel->music      = 'http://www.7qiaoban.cn/test_music.mp3';
-        $homeContentModel->record     = 'http://www.7qiaoban.cn/test_music.mp3';
-        $homeContentModel->save();
-        $homeContentItemModel             = new HomeContentItem();
-        $homeContentItemModel->content_id = $homeContentModel->id;
-        $homeContentItemModel->text       = $this->request->post('text');
-        $homeContentItemModel->save();
-
-        return json_encode(['code' => 1, 'msg' => '添加成功']);
+        $this->currentValidate->goCheck('create');
+        $result = $this->currentModel->createData($this->data);
+        if (!$result) {
+            $this->response->error(Response::UNKNOWN_ERROR);
+        }
+        return json($this->result, 201);
     }
 
-    public function saveHomeContent()
+
+    /**
+     * @return \think\response\Json
+     * @throws \app\lib\exception\BaseException
+     */
+    public function update()
     {
-
-        $saveContentData = [
-            'groupId'   => user_info('group_id'),
-            'groupType' => user_info('type'),
-            'music'     => $this->request->param('music'),
-            'record'    => $this->request->param('record',''),
-            'musicName' => $this->request->param('music_name'),
-            'items'     => $this->request->param('items'),
-        ];
-        HomeContentItem::saveContent($saveContentData);
-
-        return json($this->result);
+        $this->currentValidate->goCheck('update');
+        $result = $this->currentModel->updateData($this->data);
+        if (!$result) {
+            $this->response->error(Response::UNKNOWN_ERROR);
+        }
+        return json($this->result, 200);
     }
 
-    public function getHomeContent()
+    /**
+     * 获取首页图文详情
+     * @return \think\response\Json
+     * @throws \app\lib\exception\BaseException
+     * @throws \think\Exception
+     * @throws \think\exception\DbException
+     */
+    public function details()
     {
-
-        $getContentData       = [
-            'groupId'   => user_info('group_id'),
-            'groupType' => user_info('type'),
-            'editType'  => $this->request->param('edit_type', 0),
-        ];
-        $data                 = HomeContentItem::getContent($getContentData);
-        $this->result['data'] = $data;
-
-        return json($this->result);
-    }
-
-    public function getHomeContentItem()
-    {
-
-        $getContentItemData   = [
-            'itemId' => $this->request->param('itemId'),
-        ];
-        $data                 = HomeContentItem::getContentItem($getContentItemData);
-        $this->result['data'] = $data;
-
-        return json($this->result);
-    }
-
-    public function setCache()
-    {
-
-        $setCacheData = [
-            'itemKey'   => $this->request->param('itemKey', ''),
-            'text'      => $this->request->param('text', false),
-            'img'       => $this->request->param('img', false),
-            'music'     => $this->request->param('music', false),
-            'musicName' => $this->request->param('musicName', false),
-            'groupId'   => user_info('group_id'),
-            'groupType' => user_info('type'),
-            'type'      => $this->request->param('type', 1),
-        ];
-        HomeContentItem::setCache($setCacheData);
-
-        return json($this->result);
-    }
-
-    public function getCache()
-    {
-
-        $setCacheData         = [
-            'itemKey'   => $this->request->param('itemKey', ''),
-            'groupId'   => user_info('group_id'),
-            'groupType' => user_info('type'),
-            'type'      => $this->request->param('type', 1),
-        ];
-        $data                 = HomeContentItem::getCache($setCacheData);
-        $this->result['data'] = $data;
-
-        return json($this->result);
-    }
-
-    public function getStoreHomeContent()
-    {
-        $params       = [
-            'groupId'   => $this->data['id'],
-            'groupType' => $this->data['store_type'],
-            'editType'  => 0,
-        ];
-        $data                 = HomeContentItem::getContent($params);
-        $this->result['data'] = $data;
-
-        return json($this->result);
+        if (user_info('type') == 3 || user_info('group_id') == 0) {
+            return json(['state' => 0, 'msg' => '无数据'], 400);
+        }
+        $this->result['data'] = $this->currentModel->details();
+        if (!$this->result['data']) {
+            $this->response->error(Response::UNKNOWN_ERROR);
+        }
+        return json($this->result, 200);
     }
 }
