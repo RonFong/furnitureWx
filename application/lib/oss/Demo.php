d771212706399c051b927d3ca467abfa72be2cdc
lom <lishalom@sina.com>
2018-11-12 10:51:38 +0800

    default

---
+++ application/lib/oss/Demo.php
@@ -0,0 +1,37 @@
+<?php
+// +----------------------------------------------------------------------
+// | 深圳市保联科技有限公司
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2017 http://www.luckyins.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 黎小龙 <shalinglom@gmail.com>
+// +----------------------------------------------------------------------
+
+namespace app\lib\oss;
+
+class Demo
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * 上传
+     * @return bool
+     */
+    public function upload()
+    {
+        return 'https://www.7qiaoban.cn/multimedia/xxxxxxxxxxxx.mp4';
+    }
+
+    /**
+     * 删除
+     */
+    public function delete()
+    {
+        return true;
+    }
+}
