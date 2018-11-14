611dcf683be42073086941432377d912bea49395
lixiaolong <lishalom@sina.com>
2018-11-15 00:22:09 +0800

    default

--- application/api/controller/v1/User.php
+++ application/api/controller/v1/User.php
@@ -13,7 +13,6 @@ namespace app\api\controller\v1;
 use app\api\controller\BaseController;
 use app\api\model\User as userModel;
 use app\api\service\Wechat;
-use app\lib\enum\Response;
 use think\Request;
 use app\api\validate\User as userValidate;
 
@@ -28,79 +27,6 @@ class User extends BaseController
         $this->currentModel = new userModel();
         //当前validate
         $this->currentValidate = new userValidate();
-    }
-
-    /**
-     * 用户注册 | 更新
-     * @return \think\response\Json
-     * @throws \app\lib\exception\BaseException
-     * @throws \think\Exception
-     * @throws \think\exception\DbException
-     */
-    public function saveUser()
-    {
-
-        $this->currentValidate->goCheck('create');
-        $id = $this->currentModel->where('wx_openid', $this->data['wx_openid'])->value('id');
-        if ($id) {
-            $this->data['id'] = $id;
-        }
-        $user = $this->currentModel->saveData($this->data);
-        if ($user) {
-            $this->result['data']          = $this->currentModel->where('id', $user->id)->find()->toArray();
-            return json($this->result, 201);
-        }
-        $this->response->error(Response::USER_CREATE_ERROR);
-    }
-
-    /**
-     * 用户数据更新
-     * @throws \app\lib\exception\BaseException
-     * @throws \think\exception\DbException
-     */
-    public function update()
-    {
-
-        $this->currentValidate->goCheck('update');
-        if ($this->currentModel->saveData($this->data)) {
-            return json($this->result, 202);
-        }
-        $this->response->error(Response::USER_UPDATE_ERROR);
-    }
-
-    /**
-     * 查询用户数据
-     * @throws \app\lib\exception\BaseException
-     * @throws \think\exception\DbException
-     */
-    public function select()
-    {
-
-        $this->currentValidate->goCheck('select');
-        $map = [];
-        if ($this->data) {
-            //组装查询条件
-            $map = set_map($this->data, 'id,phone,type,group_id,group_type,wx_account');
-        }
-        $userList = $this->currentModel->selectUser($map, $this->page, $this->row);
-        if ($userList) {
-            return json($userList, 202);
-        }
-        $this->response->error(Response::USERS_EMPTY);
-    }
-
-    /**
-     * 用户删除
-     * @return \think\response\Json
-     * @throws \app\lib\exception\BaseException
-     */
-    public function delete()
-    {
-        $this->currentValidate->goCheck('delete');
-        if ($this->currentModel->deleteUser($this->data)) {
-            return json($this->result, 200);
-        }
-        $this->response->error(Response::USER_DELETE_ERROR);
     }
 
     /**
--- application/api/model/User.php
+++ application/api/model/User.php
@@ -15,43 +15,5 @@ use app\common\model\User as CoreUser;
 
 class User extends CoreUser
 {
-    /**
-     * 注册 |  更新
-     * @param $data
-     * @return false|int
-     * @throws \think\exception\DbException
-     */
-    public function saveData($data)
-    {
-        $this->data($data);
-        $this->save();
-        return $this;
-    }
-
-    /**
-     * 用户删除
-     * @param $id
-     * @return int
-     */
-    public function deleteUser($id)
-    {
-        $map['id'] = is_array($id) ? ['in', $id] : $id;
-        //TODO 事务、头像删除、信息判断
-        $result = $this->where($map)->delete();
-        return $result;
-    }
-
-    /**
-     * 查找用户
-     * @param $map
-     * @param $page
-     * @param $row
-     * @return false|\PDOStatement|string|\think\Collection
-     * @throws \think\exception\DbException
-     */
-    public function selectUser($map, $page, $row)
-    {
-        return $this->where($map)->page($page, $row)->select();
-    }
 
 }
