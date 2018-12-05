149e5e29c72c6fe7bfd4ca06434b4362816d4122
lom <lishalom@sina.com>
2018-12-05 11:43:57 +0800

    default

--- application/api/model/Article.php
+++ application/api/model/Article.php
@@ -61,7 +61,7 @@ class Article extends CoreArticle
                         $v['style'] = json_encode($v['style']);
                     }
                     $itemResult = (new ArticleContent())->save($v);
-                    if (!$itemResult) {
+                    if (empty($v['id']) && !$itemResult) {
                         exception('内容块数据写入失败：'.json_encode($v));
                     }
                 }
