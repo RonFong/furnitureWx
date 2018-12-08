b96abf1a54223240a6a73b1bf1dca444bafdd2b7
lixiaolong <lishalom@sina.com>
2018-12-08 20:38:19 +0800

    default

--- application/common/model/ShopCommodity.php
+++ application/common/model/ShopCommodity.php
@@ -17,5 +17,29 @@ namespace app\common\model;
  */
 class ShopCommodity extends Model
 {
+    /**
+     * 视频截帧
+     * @param $value
+     * @return string
+     */
+    public function getVideoSnapshotAttr($value)
+    {
+        if ($value) {
+            return $value . config('system.video_snapshot');
+        }
+        return '';
+    }
 
+    /**
+     * 高度自适应
+     * @param $value
+     * @return string
+     */
+    public function getVideoSnapshotAutoAttr($value)
+    {
+        if ($value) {
+            return $value . config('system.video_snapshot_auto');
+        }
+        return '';
+    }
 }
