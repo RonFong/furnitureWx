b5cdfabd2c211dabdaba074e08011813476f8bde
zhang <wei_06@126.com>
2018-12-15 15:31:47 +0800

    后台

--- application/lib/oss/Oss.php
+++ application/lib/oss/Oss.php
@@ -13,6 +13,7 @@ namespace app\lib\oss;
 
 use OSS\OssClient;
 use OSS\Core\OssException;
+use OSS\Core\OssUtil;
 use think\Request;
 
 class Oss
@@ -59,6 +60,18 @@ class Oss
     const resize_1000 = '?x-oss-process=image/resize,w_1000';
 
     /**
+     * 文件名称
+     * @var string
+     */
+    private $file_name = '';
+
+    /**
+     * 文件路径
+     * @var string
+     */
+    private $file_path = '';
+
+    /**
      * 错误提示
      * @var string
      */
@@ -93,17 +106,10 @@ class Oss
         }
 
         $file_info = $file->getInfo();
-        $object = 'audio/' . date('Y-m-d') . '/' . time() . rand(100, 999) . strrchr($file_info['name'], '.');// 文件名称
-        $filePath = $file_info['tmp_name'];//本地文件路径
+        $this->file_name = 'audio/' . date('Y-m-d') . '/' . time() . rand(100, 999) . strrchr($file_info['name'], '.');// 文件名称
+        $this->file_path = $file_info['tmp_name'];//本地文件路径
 
-        try {
-            $ossClient = new OssClient($this->currentConfig['accessKeyId'], $this->currentConfig['accessKeySecret'], $this->currentConfig['endpoint']);
-            $res = $ossClient->uploadFile($this->currentConfig['bucket'], $object, $filePath);
-        } catch (OssException $e) {
-            $this->error = $e->getMessage();
-            return false;
-        }
-        return $res['oss-request-url'] ? str_replace('-internal', '', $res['oss-request-url']) : '';
+        return $this->simpleUpload();
     }
 
     /**
@@ -119,17 +125,10 @@ class Oss
         }
 
         $file_info = $file->getInfo();
-        $object = 'video/' . date('Y-m-d') . '/' . time() . rand(100, 999) . strrchr($file_info['name'], '.');// 文件名称
-        $filePath = $file_info['tmp_name'];//本地文件路径
+        $this->file_name = 'video/' . date('Y-m-d') . '/' . time() . rand(100, 999) . strrchr($file_info['name'], '.');// 文件名称
+        $this->file_path = $file_info['tmp_name'];//本地文件路径
 
-        try {
-            $ossClient = new OssClient($this->currentConfig['accessKeyId'], $this->currentConfig['accessKeySecret'], $this->currentConfig['endpoint']);
-            $res = $ossClient->uploadFile($this->currentConfig['bucket'], $object, $filePath);
-        } catch (OssException $e) {
-            $this->error = $e->getMessage();
-            return false;
-        }
-        return $res['oss-request-url'] ? str_replace('-internal', '', $res['oss-request-url']) : '';
+        return $this->simpleUpload();
     }
 
     /**
@@ -146,16 +145,78 @@ class Oss
         }
 
         $file_info = $file->getInfo();
-        $object = 'image/' .$type. '/'. date('Y-m-d') . '/' . time() . rand(100, 999) . strrchr($file_info['name'], '.');// 文件名称
-        $filePath = $file_info['tmp_name'];//本地文件路径
+        $this->file_name = 'image/' .$type. '/'. date('Y-m-d') . '/' . time() . rand(100, 999) . strrchr($file_info['name'], '.');// 文件名称
+        $this->file_path = $file_info['tmp_name'];//本地文件路径
 
+
+        return $this->simpleUpload();
+    }
+
+    /**
+     * 简单上传
+     */
+    public function simpleUpload()
+    {
         try {
             $ossClient = new OssClient($this->currentConfig['accessKeyId'], $this->currentConfig['accessKeySecret'], $this->currentConfig['endpoint']);
-            $res = $ossClient->uploadFile($this->currentConfig['bucket'], $object, $filePath);
+            $res = $ossClient->uploadFile($this->currentConfig['bucket'], $this->file_name, $this->file_path);
         } catch (OssException $e) {
             $this->error = $e->getMessage();
             return false;
         }
+
+        return $res['oss-request-url'] ? str_replace('-internal', '', $res['oss-request-url']) : '';
+    }
+
+    /**
+     * 分片式上传
+     */
+    public function multiUpload()
+    {
+        try {
+            $ossClient = new OssClient($this->currentConfig['accessKeyId'], $this->currentConfig['accessKeySecret'], $this->currentConfig['endpoint']);
+            //初始化一个分片上传事件，获取uploadId。
+            $uploadId = $ossClient->initiateMultipartUpload($this->currentConfig['bucket'], $this->file_name);
+            //上传分片
+            $partSize = 5 * 1024 * 1024;//单个分片大小，默认5M
+            $uploadFileSize = filesize($this->file_path);
+            $pieces = $ossClient->generateMultiuploadParts($uploadFileSize, $partSize);
+            $responseUploadPart = array();
+            $uploadPosition = 0;
+            $isCheckMd5 = true;
+            foreach ($pieces as $i => $piece) {
+                $fromPos = $uploadPosition + (integer)$piece[$ossClient::OSS_SEEK_TO];
+                $toPos = (integer)$piece[$ossClient::OSS_LENGTH] + $fromPos - 1;
+                $upOptions = array(
+                    $ossClient::OSS_FILE_UPLOAD => $this->file_path,
+                    $ossClient::OSS_PART_NUM => ($i + 1),
+                    $ossClient::OSS_SEEK_TO => $fromPos,
+                    $ossClient::OSS_LENGTH => $toPos - $fromPos + 1,
+                    $ossClient::OSS_CHECK_MD5 => $isCheckMd5,
+                );
+                // MD5校验。
+                if ($isCheckMd5) {
+                    $contentMd5 = OssUtil::getMd5SumForFile($this->file_path, $fromPos, $toPos);
+                    $upOptions[$ossClient::OSS_CONTENT_MD5] = $contentMd5;
+                }
+                $responseUploadPart[] = $ossClient->uploadPart($this->currentConfig['bucket'], $this->file_name, $uploadId, $upOptions);
+            }
+            // $uploadParts是由每个分片的ETag和分片号（PartNumber）组成的数组。
+            $uploadParts = array();
+            foreach ($responseUploadPart as $i => $eTag) {
+                $uploadParts[] = array(
+                    'PartNumber' => ($i + 1),
+                    'ETag' => $eTag,
+                );
+            }
+
+            //完成上传，OSS将把这些分片组合成一个完整的文件。
+            $res = $ossClient->completeMultipartUpload($this->currentConfig['bucket'], $this->file_name, $uploadId, $uploadParts);
+        } catch (OssException $e) {
+            $this->error = $e->getMessage();
+            return false;
+        }
+
         return $res['oss-request-url'] ? str_replace('-internal', '', $res['oss-request-url']) : '';
     }
 
