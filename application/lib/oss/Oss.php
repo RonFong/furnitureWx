e0fb41e8f4eeba395c0c6d546105c55339ce469a
lixiaolong <lishalom@sina.com>
2018-12-16 17:02:26 +0800

    default

--- application/lib/oss/Oss.php
+++ application/lib/oss/Oss.php
@@ -21,79 +21,6 @@ class Oss
 {
     private $currentConfig = [];
 
-    //图片缩略尺寸
-    const imgThumbSize = [
-        //门店
-        'shop'      => [
-            'small'   => [           //缩略图中的小号图
-                'w'     => [          //宽度尺寸
-                    'ratio' => 4,     //宽高比例
-                    'value' => 480    //值
-                ],
-                'h'     => [
-                    'ratio' => 3,
-                    'value' => 360
-                ],
-            ],
-            'large'  => [           //缩略图中的大号图
-                'w'     => [
-                    'ratio' => 16,
-                    'value' => 1080
-                ],
-                'h'     => [
-                    'ratio' => 9,
-                    'value' => 607
-                ]
-            ]
-        ],
-        //文章
-        'article'   => [
-            'small'  => [
-                'w'     => [
-                    'ratio' => 4,
-                    'value' => 500
-                ],
-                'h'     => [
-                    'ratio' => 3,
-                    'value' => 375
-                ]
-            ],
-            'large'   => [
-                'w'     => [
-                    'ratio' => 0,       // 等比缩放
-                    'value' => 1000
-                ],
-                'h'     => [
-                    'ratio' => 0,
-                    'value' => 0
-                ]
-            ]
-        ],
-        //默认
-        'default'   => [
-            'small'  => [
-                'w'     => [
-                    'ratio' => 1,
-                    'value' => 400
-                ],
-                'h'     => [
-                    'ratio' => 1,
-                    'value' => 400
-                ]
-            ],
-            'large'  => [
-                'w'     => [
-                    'ratio' => 4,
-                    'value' => 400
-                ],
-                'h'     => [
-                    'ratio' => 3,
-                    'value' => 300
-                ]
-            ]
-        ]
-    ];
-
 
     /**
      * 文件名称
@@ -269,7 +196,7 @@ class Oss
      */
     private function calculateImgThumbSize($imgInfo, $type, $size)
     {
-        $sizeConfig = self::imgThumbSize[$type];
+        $sizeConfig = config('image')[$type];
         $width = $imgInfo->width();
         $height = $imgInfo->height();
 
