c98f80903f73db3c2203d007f45920c5d5ddc7da
lom <lishalom@sina.com>
2018-12-16 16:50:28 +0800

    Update Oss.php

--- application/lib/oss/Oss.php
+++ application/lib/oss/Oss.php
@@ -14,35 +14,86 @@ namespace app\lib\oss;
 use OSS\OssClient;
 use OSS\Core\OssException;
 use OSS\Core\OssUtil;
+use think\Image;
 use think\Request;
 
 class Oss
 {
     private $currentConfig = [];
 
-    /**
-     * 先缩放：宽1080，再裁剪：高607（16:9）
-     * @var string
-     */
-    const resize_1080 = '?x-oss-process=image/resize,w_1080/crop,h_607,g_center';
+    //图片缩略尺寸
+    const imgThumbSize = [
+        //门店
+        'shop'      => [
+            'small'   => [           //缩略图中的小号图
+                'w'     => [          //宽度尺寸
+                    'ratio' => 4,     //宽高比例
+                    'value' => 480    //值
+                ],
+                'h'     => [
+                    'ratio' => 3,
+                    'value' => 360
+                ],
+            ],
+            'large'  => [           //缩略图中的大号图
+                'w'     => [
+                    'ratio' => 16,
+                    'value' => 1080
+                ],
+                'h'     => [
+                    'ratio' => 9,
+                    'value' => 607
+                ]
+            ]
+        ],
+        //文章
+        'article'   => [
+            'small'  => [
+                'w'     => [
+                    'ratio' => 4,
+                    'value' => 500
+                ],
+                'h'     => [
+                    'ratio' => 3,
+                    'value' => 375
+                ]
+            ],
+            'large'   => [
+                'w'     => [
+                    'ratio' => 0,       // 等比缩放
+                    'value' => 1000
+                ],
+                'h'     => [
+                    'ratio' => 0,
+                    'value' => 0
+                ]
+            ]
+        ],
+        //默认
+        'default'   => [
+            'small'  => [
+                'w'     => [
+                    'ratio' => 1,
+                    'value' => 400
+                ],
+                'h'     => [
+                    'ratio' => 1,
+                    'value' => 400
+                ]
+            ],
+            'large'  => [
+                'w'     => [
+                    'ratio' => 4,
+                    'value' => 400
+                ],
+                'h'     => [
+                    'ratio' => 3,
+                    'value' => 300
+                ]
+            ]
+        ]
+    ];
 
-    /**
-     * 缩放，宽1000 高auto
-     * @var string
-     */
-    const resize_1000 = '?x-oss-process=image/resize,m_lfit,w_1000';
-
-    /**
-     * 先缩放：宽500，再裁剪：高375（4:3）
-     * @var string
-     */
-    const resize_500 = '?x-oss-process=image/resize,w_500/crop,h_375,g_center';
-
-    /**
-     * 先缩放：宽480，再裁剪：高360（4:3）
-     * @var string
-     */
-    const resize_480 = '?x-oss-process=image/resize,w_480/crop,h_360,g_center';
 
     /**
      * 文件名称
@@ -96,8 +147,9 @@ class Oss
         $file_info = $file->getInfo();
         $this->file_name = 'audio/' . date('Y-m-d') . '/' . time() . rand(100, 999) . strrchr($file_info['name'], '.');// 文件名称
         $this->file_path = $file_info['tmp_name'];//本地文件路径
-        //若大于5M，则采用分片式上传
-        return $file_info['size'] > 5242880 ? $this->multiUpload() : $this->simpleUpload();
+
+        return $this->simpleUpload();
+//        return $this->multiUpload();
     }
 
     /**
@@ -116,8 +168,8 @@ class Oss
         $this->file_name = 'video/' . date('Y-m-d') . '/' . time() . rand(100, 999) . strrchr($file_info['name'], '.');// 文件名称
         $this->file_path = $file_info['tmp_name'];//本地文件路径
 
-        //若大于5M，则采用分片式上传
-        return $file_info['size'] > 5242880 ? $this->multiUpload() : $this->simpleUpload();
+//        return $this->simpleUpload();
+        return $this->multiUpload();
     }
 
     /**
@@ -133,12 +185,21 @@ class Oss
             return false;
         }
 
+        $imgInfo = Image::open($this->request->file('file'));
+
+        //计算缩略图宽高
+        $smallSize = $this->calculateImgThumbSize($imgInfo, $type, 'small');
+        $largeSize = $this->calculateImgThumbSize($imgInfo, $type, 'large');
+
         $file_info = $file->getInfo();
         $this->file_name = 'image/' .$type. '/'. date('Y-m-d') . '/' . time() . rand(100, 999) . strrchr($file_info['name'], '.');// 文件名称
         $this->file_path = $file_info['tmp_name'];//本地文件路径
 
-        //若大于5M，则采用分片式上传
-        return $file_info['size'] > 5242880 ? $this->multiUpload() : $this->simpleUpload();
+        return [
+            'img'               =>  $this->simpleUpload(),
+            'img_thumb_small'   => $smallSize == false ? $this->simpleUpload() :  $this->simpleUpload() . $smallSize,
+            'img_thumb_large'   => $largeSize == false ? $this->simpleUpload() : $this->simpleUpload() . $largeSize
+        ];
     }
 
     /**
@@ -198,4 +259,65 @@ class Oss
     {
         return $this->error;
     }
+
+    /**
+     * 计算缩略图宽高值
+     * @param $imgInfo   object   图片信息
+     * @param $type    string 类型   shop | article | default
+     * @param $size    string   尺码  small | large
+     * @return string
+     */
+    private function calculateImgThumbSize($imgInfo, $type, $size)
+    {
+        $sizeConfig = self::imgThumbSize[$type];
+        $width = $imgInfo->width();
+        $height = $imgInfo->height();
+
+        if ($sizeConfig[$size]['w']['ratio'] == 0) {
+            // 等比缩放
+            if ($width <= $sizeConfig[$size]['w']['value']) {
+                //原图小于等于所设尺寸，直接使用原图，不生成缩略图
+                return false;
+            } else {
+                $w = $sizeConfig[$size]['w']['value'];
+                $h = round($w / $width * $height);
+            }
+            $resizeSize = "w_$w";
+            $cropSize = "h_$h";
+
+        } elseif ($width <= $height) {
+            //取宽高值中较小的值来作为缩放基数
+            if ($width < $sizeConfig[$size]['w']['value']) {
+                $w = $width;        //小于标准宽度，不对宽度进行缩放
+            } else {
+                $w = $sizeConfig[$size]['w']['value'];    //大于标准宽度，则缩放至标准宽度
+            }
+            $resizeSize = "w_$w";
+            $h = round($w / $sizeConfig[$size]['w']['ratio'] * $sizeConfig[$size]['h']['ratio']);
+            $cropSize = "h_$h";
+        } else {
+            if ($height < $sizeConfig[$size]['h']['value']) {
+                $h = $height;
+            } else {
+                $h = $sizeConfig[$size]['h']['value'];
+            }
+            $resizeSize = "h_$h";
+            $w = round($h / $sizeConfig[$size]['h']['ratio'] * $sizeConfig[$size]['w']['ratio']);
+            $cropSize = "w_$w";
+        }
+        return $this->setImgSize($resizeSize, $cropSize);
+    }
+
+    /**
+     * 获取图片缩放值
+     * @param $resizeSize string  缩放
+     * @param $cropSize   string  裁剪
+     * @return string
+     */
+    private function setImgSize($resizeSize, $cropSize)
+    {
+        return "?x-oss-process=image/resize,m_lfit,$resizeSize/crop,x_0,y_0,$cropSize,g_center";
+    }
+
+
 }
