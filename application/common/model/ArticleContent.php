3f64853f0bc3c5772e64c7adf9edac56e18d01d7
lom <lishalom@sina.com>
2018-12-21 17:23:28 +0800

    default

--- application/common/model/ArticleContent.php
+++ application/common/model/ArticleContent.php
@@ -42,4 +42,27 @@ class ArticleContent extends Model
         }
         return '';
     }
+
+    public function getStyleAttr($value)
+    {
+        if ($value) {
+            return json_decode($value);
+        }
+        return '';
+    }
+
+    /**
+     * Emoji 表情符
+     * @param $value
+     * @return string
+     */
+    public function getTextAttr($value)
+    {
+        return $this->emojiDecode($value);
+    }
+
+    public function setTextAttr($value)
+    {
+        return $this->emojiEncode($value);
+    }
 }
