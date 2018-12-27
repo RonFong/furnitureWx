225e856157d32525ef5b033ec58fb0908ca21637
lom <lishalom@sina.com>
2018-12-27 10:30:41 +0800

    default

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '47.98.223.33',
+    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'furniture',
+    'database'        => 'default',
     // 用户名
-    'username'        => 'user',
+    'username'        => 'default',
     // 密码
-    'password'        => 'lucky2018',
+    'password'        => 'default',
     // 端口
     'hostport'        => '',
     // 连接dsn
