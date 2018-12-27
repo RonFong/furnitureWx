4abc73696ed572469f59e00a0c6c790c6579bd26
lom <lishalom@sina.com>
2018-12-27 11:00:44 +0800

    default

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'default',
+    'database'        => 'furniture',
     // 用户名
-    'username'        => 'default',
+    'username'        => 'user',
     // 密码
-    'password'        => 'default',
+    'password'        => 'lucky2018',
     // 端口
     'hostport'        => '',
     // 连接dsn
