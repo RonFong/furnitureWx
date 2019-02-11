a8fc46fd264c27b9e47b28bb5f3818938ed2733d
lom <lishalom@sina.com>
2019-02-11 16:58:04 +0800

    Update database.php

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'furniture',
+    'database'        => '',
     // 用户名
-    'username'        => 'user',
+    'username'        => '',
     // 密码
-    'password'        => 'lucky2018',
+    'password'        => '',
     // 端口
     'hostport'        => '',
     // 连接dsn
