27162b8ff0af62fde02a32670b616c85290aa062
lixiaolong <lishalom@sina.com>
2019-02-20 22:08:52 +0800

    default

---
+++ application/common/model/FactoryProductClassify.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 黎小龙 <shalinglom@gmail.com>
+// +----------------------------------------------------------------------
+// | CreateTime: 2019/2/20 
+// +----------------------------------------------------------------------
+
+
+namespace app\common\model;
+
+
+/**
+ * 厂家产品
+ * Class FactoryProductClassify
+ * @package app\common\model
+ */
+class FactoryProductClassify extends Model
+{
+
+}
