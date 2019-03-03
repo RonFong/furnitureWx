86e2d73cb16debe52bf9d34e9e0103ccc14404dc
lixiaolong <lishalom@sina.com>
2019-03-03 17:00:42 +0800

    default

--- application/common/model/ProductColor.php
+++ application/common/model/ProductColor.php
@@ -1,18 +1,19 @@
 <?php
 // +----------------------------------------------------------------------
-// | 深圳市保联科技有限公司
-// +----------------------------------------------------------------------
-// | Copyright (c) 2013-2017 http://www.luckyins.com All rights reserved.
-// +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: 黎小龙 <shalinglom@gmail.com>
 // +----------------------------------------------------------------------
+// | CreateTime: 2019/3/3 
+// +----------------------------------------------------------------------
+
 
 namespace app\common\model;
 
 
-class StoreClassifyProperty extends Model
-{
+use traits\model\SoftDelete;
 
+class ProductColor extends Model
+{
+    use SoftDelete;
 }
