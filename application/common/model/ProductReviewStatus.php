0f32f95f3355b7139e24a7bb98ea4b619fbb9d36
lixiaolong <lishalom@sina.com>
2019-03-03 18:05:44 +0800

    default

--- application/common/model/ProductReviewStatus.php
+++ application/common/model/ProductReviewStatus.php
@@ -14,4 +14,27 @@ namespace app\common\model;
 class ProductReviewStatus extends Model
 {
 
+    protected $remark = [
+        0   => '99家服务人员将尽快处理，请稍等~',
+        1   => '已同步到商城，输入编号，就可找到这款产品哦~',
+    ];
+
+    /**
+     * 写入审核进度
+     * @param $productId
+     * @param $status
+     * @param string $remark
+     * @return bool
+     * @throws \think\exception\DbException
+     */
+    public function write($productId, $status, $remark = '')
+    {
+        self::save([
+            'product_id'    => $productId,
+            'status'        => $status,
+            'remark'        => $remark ?? $this->remark[$status]
+        ]);
+        return true;
+    }
+
 }
