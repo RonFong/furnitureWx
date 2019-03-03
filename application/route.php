ebf3925bdcdf4fe0ded2b8418c935714db481914
lixiaolong <lishalom@sina.com>
2019-03-03 17:35:09 +0800

    default

--- application/route.php
+++ application/route.php
@@ -170,7 +170,7 @@ Route::group('api/:version', function () {
         Route::post('create', 'api/:version.product/create');
         //修改产品信息
         Route::put('update', 'api/:version.product/update');
-        //产品零售价计算比例
+        //根据分类获取产品列表
         Route::get('getListByClassify', 'api/:version.product/getListByClassify');
         //产品零售价计算比例
         Route::get('retailPriceRatio', 'api/:version.product/retailPriceRatio');
