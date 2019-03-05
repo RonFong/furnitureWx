9d733db6b5c58e00437877bbbfa7ea574079712d
lom <lishalom@sina.com>
2019-03-05 13:59:23 +0800

    default

--- application/common/model/Factory.php
+++ application/common/model/Factory.php
@@ -10,16 +10,6 @@ class Factory extends Model
     use SoftDelete;
 
     protected $hidden = [
-        "license_img",
-        "factory_wx",
-        "factory_contact",
-        "factory_phone",
-        "factory_province",
-        "factory_city",
-        "factory_district",
-        "deliver_province",
-        "deliver_city",
-        "deliver_district",
         "create_by",
         "create_time",
         "update_by",
