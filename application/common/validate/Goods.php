5b830db420790a1e7a9b9984d768b4dd73faf814
lom <lishalom@sina.com>
2019-04-12 18:52:49 +0800

    default

--- application/common/validate/Goods.php
+++ application/common/validate/Goods.php
@@ -19,13 +19,17 @@ namespace app\common\validate;
 class Goods extends BaseValidate
 {
     protected $rule = [
-        'shop_id'   => 'require|number',
-        'page'      => 'require|number',
-        'row'       => 'require|number',
+        'shop_id'       => 'require|number',
+        'product_id'    => 'require|number',
+        'page'          => 'require|number',
+        'row'           => 'require|number',
+        'configure_id'  => 'require|number',
+        'retail_price'  => 'require|number',
     ];
 
     protected $message = [
-
+        'retail_price.require'  => '请填写自定义零售价',
+        'retail_price.number'   => '零售价必须为整数',
     ];
 
     protected $scene = [
@@ -34,5 +38,30 @@ class Goods extends BaseValidate
             'page',
             'row'
         ],
+        'info'  => [
+            'shop_id',
+            'product_id'
+        ],
+        'setRetailPrice'    => [
+            'shop_id'   => 'require|number|isCurrentShop',
+            'product_id',
+            'configure_id',
+            'retail_price',
+        ],
     ];
+
+    /**
+     * 判断当前用户是否为 商城入口商家
+     * @param $value
+     * @param $rule
+     * @param $data
+     * @return bool|string
+     */
+    protected function isCurrentShop($value, $rule, $data)
+    {
+        if (user_info('type') != 2 || user_info('group_id') != $value) {
+            return '非当前商家';
+        }
+        return true;
+    }
 }
