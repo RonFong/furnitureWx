35f37c3a14545f21bc12ab6fe743ff6c640b553e
50xiuer <FiftyW50@163.com>
2019-04-30 13:38:43 +0800

    累加用户登录次数

--- application/api/service/Token.php
+++ application/api/service/Token.php
@@ -89,7 +89,7 @@ class Token
         } else {
             $locationData['user_id'] = $userInfo->id;
             //累加登录次数
-            (new User())->where('id', $userInfo->id)->setInc('login_num', 1, 10);
+            (new User())->where('id', $userInfo->id)->setInc('login_num');
             $result = $userInfo->toArray();
         }
         if (!empty($locationData['lat']) && !empty($locationData['lng'])) {
