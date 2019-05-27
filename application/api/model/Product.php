406d804bb9dcba3e22b64c3a2f4cc20e45a2fcf1
lixiaolong <lishalom@sina.com>
2019-05-28 00:28:42 +0800

    default

--- application/api/model/Product.php
+++ application/api/model/Product.php
@@ -163,7 +163,7 @@ class Product extends CoreProduct
 
         $otherInfo = Db::table('factory')
             ->where('id', $info['factory_id'])
-            ->field('factory_province, factory_city, factory_district, deliver_province, deliver_city, deliver_district')
+            ->field('factory_province, factory_city, factory_district, sales_province, sales_city, sales_district')
             ->find();
         $info['deliver_address'] = $otherInfo['sales_province'] . ' ' . $otherInfo['sales_city'] . ' ' . $otherInfo['sales_district'];
         $info['factory_address'] = $otherInfo['factory_province'] . ' ' . $otherInfo['factory_city'] . ' ' . $otherInfo['factory_district'];
