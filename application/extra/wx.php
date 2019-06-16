c73e8b85e7286b2a51eaad8506d8c196202befb7
lishalom <lishalom@sina.com>
2019-06-16 13:48:08 +0800

    default

--- application/extra/wx.php
+++ application/extra/wx.php
@@ -10,16 +10,15 @@
 // +----------------------------------------------------------------------
 
 return [
-    //公众号 AppID
-
-    //公众号 AppSecret
-
-
-    //小程序 AppID
-
-    //小程序 AppSecret
-
-
-    //SMS
-
+    //公众号 AppID	    //公众号 AppID
+    'gzh_app_id'            => 'wxa6d691299093f1a4',
+    //公众号 AppSecret	    //公众号 AppSecret
+    'gzh_app_secret'        => '7a91a512262ec4a9dea5a507612e48a0',
+    //小程序 AppID	    //小程序 AppID
+    'xcx_app_id'            => 'wx195a5e8ed1a55ead',
+    //小程序 AppSecret	    //小程序 AppSecret
+    'xcx_app_secret'        => 'd0a065f66e34734712f8b4310691b5c3',
+    //SMS	    //SMS
+    'sms_app_id'            => '1400108281',
+    'sms_app_key'           => 'fe504050bd4bcaed5651ed2f1a093611'
 ];
