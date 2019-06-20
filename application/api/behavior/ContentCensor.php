6b60aebe14d793d8ced1337fdfe608e1142f1381
lishalom <lishalom@sina.com>
2019-06-20 15:46:41 +0800

    default

--- application/api/behavior/ContentCensor.php
+++ application/api/behavior/ContentCensor.php
@@ -16,28 +16,31 @@ class ContentCensor
     {
         try {
             $param = Request::instance()->param();
-            //img 信息无需审核
-            $imgKey = ['img', 'img_thumb_small', 'img_thumb_large'];
+            unset($param['version']);
+            if (!empty($param)) {
+                //img 信息无需审核
+                $imgKey = ['img', 'img_thumb_small', 'img_thumb_large'];
 
-            $joinStr = function ($param, & $allStr) use (& $joinStr, $imgKey) {
-                foreach ($param as $k => $v) {
-                    if (is_array($v)) {
-                        $joinStr($v, $allStr);
-                    } else {
-                        if (!in_array($k, $imgKey)) {
-                            $allStr .= $v;
+                $joinStr = function ($param, & $allStr) use (& $joinStr, $imgKey) {
+                    foreach ($param as $k => $v) {
+                        if (is_array($v)) {
+                            $joinStr($v, $allStr);
+                        } else {
+                            if (!in_array($k, $imgKey)) {
+                                $allStr .= $v;
+                            }
                         }
                     }
+                    return $allStr;
+                };
+
+                $allStr = '';
+                $allStr = $joinStr($param, $allStr);
+                $result = \app\lib\baiduAI\ContentCensor::text($allStr);
+
+                if ($result['state'] == 1 && $result['hitTag'] != 4) {
+                    exception('您提交的内容中包含' . $result['msg'] . '信息，请修改后提交');
                 }
-                return $allStr;
-            };
-
-            $allStr = '';
-            $allStr = $joinStr($param, $allStr);
-            $result = \app\lib\baiduAI\ContentCensor::text($allStr);
-
-            if ($result['state'] = 1 && $result['hitTag'] != 4) {
-                exception('您提交的内容中包含'.$result['msg'].'信息，请修改后提交');
             }
         } catch (\Exception $e) {
             die(json_encode(['state' => 0, 'errorCode' => 1003, 'msg' => $e->getMessage()]));
